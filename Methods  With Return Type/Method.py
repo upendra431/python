e8625b333ce12991e5da7ac610bb6b4b61edefd8
upendra431 <uppu431@gmail.com>
2020-04-13 00:32:08 +0530

    Methods  With Return Type

---
+++ Methods  With Return Type/Method.py
@@ -0,0 +1,17 @@
+""" 
+Method syn:
+def function_name ( arg1, arg2, ...) : 
+	...... 
+	# function body 
+	...... 
+    
+"""
+def my_method():
+    print("Hello Python")
+def my_method_with_input(msg):
+    print(msg)
+
+#Calling method using method/function name
+my_method()
+#Calling method using method/function name and passing the input to the method
+my_method_with_input("Hello i am method input")
---
+++ Methods  With Return Type/MethodReturnValue.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+    #below return stmt will return the sum of two numbers
+    return a+b
+
+print(sum(2,3))
+
