aee58adb9da9d21eafc518e501e3e1c8e82de0aa
upendra431 <uppu431@gmail.com>
2020-04-13 01:22:11 +0530

    Getting User input using input method, reading input from user convertig into number or int

---
+++ Getting User input using input method/ReadingNumbers.py
@@ -0,0 +1,13 @@
+#Reading the name as a String 
+name=input("Enter your name: ")
+#Reading as a String converting into int using int() function
+age=int(input("Enter your age: "))
+
+print("Your Name is {n} , age is {a} .".format(n=name,a=age))
+
+"""
+O/P:
+Enter your name: Ram
+Enter your age: 20
+Your Name is Ram , age is 20 .
+"""
