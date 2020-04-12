ccc426176f6fb356e98614b2e11a5d277d5b84c7
upendra431 <uppu431@gmail.com>
2020-04-13 03:11:54 +0530

    If Else Stmt/

---
+++ If Else Stmt/IfElseSTMT.py
@@ -0,0 +1,8 @@
+def check_if_or_else(age):
+    if age>=18:
+        print("Riht to vote! {} years  age people.".format(age))
+    else:
+        print("{} years Not a Right age to vote".format(age))
+
+check_if_or_else(18)
+check_if_or_else(10)
