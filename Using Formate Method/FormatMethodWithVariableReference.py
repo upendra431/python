1cc70786474b83e6fe4c866f3b34b848fa1591bd
upendra431 <uppu431@gmail.com>
2020-04-13 00:57:31 +0530

    Using Formate Method

---
+++ Using Formate Method/FormatMethodWithVariableReference.py
@@ -0,0 +1,5 @@
+empid=120
+name='Ram'
+age='27'
+#Using the refrence variable.
+print("Employee id {empid} name is {name} age is {age}".format(name=name,age=age,empid=empid))
---
+++ Using Formate Method/UsingFormateMethod.py
@@ -0,0 +1,12 @@
+
+#to avoid concat operation we can use format method 
+print("This year "+str(2020)+" corona year")
+#using format method will replace the value in the place of {}
+print("This year {} corona year".format(2020))
+
+"""
+O/P:
+
+This year 2020 corona year
+This year 2020 corona year
+"""
